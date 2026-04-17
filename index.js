4dac61288b2e27bf5d87135af2a75f9f19c4f76a
Trevor <mtrevorkk@gmail.com>
2026-04-18 00:57:36 +0300

    attempt15

--- index.js
+++ index.js
@@ -4,3 +4,30 @@
 module.exports = {
   ...(typeof createLoginTracker !== 'undefined' && { createLoginTracker })
 };
+function createLoginTracker(userinfo) {
+  let attemptCount = 0;
+
+  const innerfunction = (passwordAttempt) => {
+    attemptCount++;
+
+    if (attemptCount > 3) {
+      return "Account locked due to too many failed attempts";
+    } else if (passwordAttempt === userinfo.password) {
+      return "Login successful";
+    } else {
+      return `Attempt ${attemptCount}: Login failed`;
+    }
+  };
+
+  return innerfunction;
+}
+
+// usage
+const login = createLoginTracker({
+  username: "Trevor",
+  password: "password123"
+});
+
+console.log(login("wrong"));
+console.log(login("wrong"));
+console.log(login("password123"));
